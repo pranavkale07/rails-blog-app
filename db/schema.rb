6e7989a872ba252fb648907115b775625ecb358b
pranavkale07 <pranavkale911@gmail.com>
2025-03-31 06:38:49 +0000

    Switched database from SQLite to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_02_24_124953) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
